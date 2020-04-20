72af586e5a7f4808ec09806058aa669cc4860dc6
Heather Moreira <moreiraheather@gmail.com>
2020-04-20 23:38:09 +0000

    changes to methods in Scraper class

--- lib/swell_retreats/scraper.rb
+++ lib/swell_retreats/scraper.rb
@@ -4,15 +4,15 @@ require 'open-uri'
 require 'pry'
 
 class SwellRetreats::Scraper
-  attr_accessor :name, :location, :dates, :description, :single_price, :double_price, :availability, :url   
+  attr_accessor :name, :location, :dates, :description, :single_price, :double_price, :availability, :url 
   
-  #iterates over retreats on swellpage and creates a nested retreat_hash for each retreat and pushes it into the retreats_array 
-  def self.scrape_retreat_page(sweLl_site)
+ 
+  def self.scrape_homepage(site)
     #swell_site = "https://www.swellwomen.com/portfolios/coaching-retreats/"
     page = Nokogiri::HTML(open(site))
     retreats_array = []
     page.css("div.inner").collect do |retreat| 
-      retreat_hashs = {
+      retreats_hash = {
         :name => retreat.css("td.rs-title").text,
         :dates => retreat.css("td.rs-dates").text,
         :availability => retreat.css("td.rs-availability-words").text, 
@@ -28,24 +28,16 @@ class SwellRetreats::Scraper
 #scrapes a retreats page and creates a hash of retreat_details 
   def self.scrape_retreat_details(retreat_url) 
     page = Nokogiri::HTML(open(retreat_url))
-    retreat_details_hash = {
-    :name => page.css(), 
-    :location => page.css().text,
-    :description => page.css().text,
-    :single_price => page.css(),
-    :double_price => page.css()
- 
+    retreat_details_hash = {}
+    retreat_details_hash[:program_name]= page.css(), 
+    retreat_details_hash[:accomodations] = page.css().text,
+    retreat_details_hash[:description] = page.css().text,
+    retreat_details_hash[:single_price] = page.css(),
+    retreat_details_hash[:double_price] = page.css()
     }
+    retreat_details_hash 
   end   
   
-  # def self.make_retreats #iterate over retreats_array to create new objects. call in CLI#list_retreats
-  #   self.scrape_home_page.collect do |retreat|
-  #     puts "#{retreat[:name]}"
-  #   end 
-  # end
-  
-  
-
 
 end 
 
