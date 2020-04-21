03292e4fcd328b89e2854dee04cd30bcb0516b42
Heather Moreira <moreiraheather@gmail.com>
2020-04-21 03:46:17 +0000

    built Retreat#see_more_details

--- lib/swell_retreats/retreat.rb
+++ lib/swell_retreats/retreat.rb
@@ -14,19 +14,13 @@ class Retreat
     #which sends it to initialize where each key/value pair is assigned as the objects attributes, AND adds the retreat to @@all array so all retreat objects are in @@all. make sure the attr_accessors match
   end 
   
-  def self.see_more_details(attributes_hash)
-    retreat_hash = results from Scraper.scrape_retreat_details #which is a hash of key/values representing the retreats info 
-    iterate over the retreat hash  to list more details about retreat 
-    # puts "retreat_details_hash[:program_name]" 
-    # puts "retreat_details_hash[:accomodations]"
-    # puts "retreat_details_hash[:description]"
-    # puts "retreat_details_hash[:single_price]"
-    # puts "retreat_details_hash[:double_price]"
-    #retreat_hash.each do |key, value| self.send(("#{key}:"), value)}
+  def see_more_details(attributes_hash)
+    #attribute_hash = results from Scraper.scrape_retreat_details 
+    attribute_hash.each{|key, value| self.send(("#{key}="), value)}
   end 
   
   def self.all 
-    @@all.uniq 
+    @@all
   end
   
 
